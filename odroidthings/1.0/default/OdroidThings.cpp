ee3830c5300adb35c5263e7a9fd7bbf331df189a
Luke Go <sangch.go@gmail.com>
2020-01-09 13:46:31 +0900

    ODROID: things: interfaces: Add license banner.
    Signed-off-by: Luke Go <sangch.go@gmail.com>
    Change-Id: If343a1418929ac37f27dd438ab16ac126640f835

--- odroidthings/1.0/default/OdroidThings.cpp
+++ odroidthings/1.0/default/OdroidThings.cpp
@@ -1,3 +1,21 @@
+/*
+ *    Copyright (c) 2019 Sangchul Go <luke.go@hardkernel.com>
+ *
+ *    OdroidThings is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Lesser General Public License as
+ *    published by the Free Software Foundation, either version 3 of the
+ *    License, or (at your option) any later version.
+ *
+ *    OdroidThings is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public
+ *    License along with OdroidThings.
+ *    If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #define LOG_TAG "vendor.hardkernel.hardware.odroidthings@1.0-hal"
 
 #include <log/log.h>
